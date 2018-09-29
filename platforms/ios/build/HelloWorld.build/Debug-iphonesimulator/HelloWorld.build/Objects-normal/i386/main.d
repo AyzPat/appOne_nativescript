09fb5fa3026f74594c260eabdc3f4ed576a4f4bb
ayz14 <ayzpat@gmail.com>
2018-09-30 00:05:47 +0530

    Initial commit

---
+++ platforms/ios/build/HelloWorld.build/Debug-iphonesimulator/HelloWorld.build/Objects-normal/i386/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/Sayz/Documents/nativescript/natng/HelloWorld/platforms/ios/HelloWorld/HelloWorld-Prefix.pch \
+  /Users/Sayz/Documents/nativescript/natng/HelloWorld/platforms/ios/internal/main.m \
+  /Users/Sayz/Documents/nativescript/natng/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/Sayz/Documents/nativescript/natng/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/Sayz/Documents/nativescript/natng/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/Sayz/Documents/nativescript/natng/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/Sayz/Documents/nativescript/natng/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/Sayz/Documents/nativescript/natng/HelloWorld/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/Sayz/Documents/nativescript/natng/HelloWorld/platforms/ios/internal/TNSDebugging.h \
+  /Users/Sayz/Documents/nativescript/natng/HelloWorld/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
